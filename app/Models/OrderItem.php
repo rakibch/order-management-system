94eb9d24dd8e79ef0a42b97fed5883e7f8a81e78
rakibch <rakib9204@gmail.com>
2025-11-18 12:03:59 +0600

    set code for order item

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    protected $fillable = [
+        'order_id','product_variant_id','product_name','unit_price','quantity','total_price','meta'
+    ];
+
+    protected $casts = [
+        'meta' => 'array',
+        'unit_price' => 'decimal:2',
+        'total_price' => 'decimal:2',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function variant()
+    {
+        return $this->belongsTo(ProductVariant::class, 'product_variant_id');
+    }
 }
