4d0a726f84f36239f45b237b96ded8f83b6d2ca7
rakibch <rakib9204@gmail.com>
2025-11-18 11:57:52 +0600

    set product variant model

--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -3,8 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductVariant extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'product_id','sku','attributes','price','barcode','active'
+    ];
+
+    protected $casts = [
+        'attributes' => 'array',
+        'price' => 'decimal:2',
+        'active' => 'boolean',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function inventory()
+    {
+        return $this->hasOne(Inventory::class, 'product_variant_id');
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'product_variant_id');
+    }
 }
