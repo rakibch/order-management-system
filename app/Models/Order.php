87ce40fe5e8ded544b34376b633ff683b107f2d2
rakibch <rakib9204@gmail.com>
2025-11-18 12:02:43 +0600

    set order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'order_number','user_id','vendor_id','status','subtotal','shipping','tax','total',
+        'billing_address','shipping_address','meta','placed_at','processed_at'
+    ];
+
+    protected $casts = [
+        'billing_address' => 'array',
+        'shipping_address' => 'array',
+        'meta' => 'array',
+        'placed_at' => 'datetime',
+        'processed_at' => 'datetime',
+        'subtotal' => 'decimal:2',
+        'shipping' => 'decimal:2',
+        'tax' => 'decimal:2',
+        'total' => 'decimal:2',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(\App\Models\User::class);
+    }
+
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
