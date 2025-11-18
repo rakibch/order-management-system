30abac43a45eb65836c75a4b8673c9717e4796c1
rakibch <rakib9204@gmail.com>
2025-11-18 11:58:39 +0600

    set inventory model

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
-    //
+    protected $fillable = [
+        'product_variant_id','stock','reserved','low_stock_threshold','location'
+    ];
+
+    public function variant()
+    {
+        return $this->belongsTo(ProductVariant::class, 'product_variant_id');
+    }
+
+    public function isLow()
+    {
+        return $this->stock <= $this->low_stock_threshold;
+    }
 }
