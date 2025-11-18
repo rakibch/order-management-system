b3ba713ac27e84ea74b608e2eeefdff1c6bcd82e
rakibch <rakib9204@gmail.com>
2025-11-18 12:02:05 +0600

    set product image model

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class ProductImage extends Model
 {
-    //
+    protected $fillable = [
+        'product_id','path','alt','sort_order','is_primary'
+    ];
+
+    protected $casts = [
+        'is_primary' => 'boolean',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function getUrlAttribute()
+    {
+        return Storage::url($this->path);
+    }
 }
