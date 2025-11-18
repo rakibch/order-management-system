249a255605ed5fd0e44fbcfa67c6770f88fbadcc
rakibch <rakib9204@gmail.com>
2025-11-18 12:04:43 +0600

    set refresh token code

--- app/Models/RefreshToken.php
+++ app/Models/RefreshToken.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class RefreshToken extends Model
 {
-    //
+    protected $fillable = [
+        'user_id','token_hash','device','ip','expires_at'
+    ];
+
+    protected $casts = [
+        'expires_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
